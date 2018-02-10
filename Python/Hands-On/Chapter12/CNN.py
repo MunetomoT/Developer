0e3f7511a902f149e7115919283a576aed196f91
MunetomoT <Munetomo.T@btinternet.com>
2018-02-10 15:31:09 +0900

    Add

--- Python/Hands-On/Chapter12/CNN.py
+++ Python/Hands-On/Chapter12/CNN.py
@@ -6,21 +6,24 @@ X = tf.placeholder(tf.float32, shape = (None, 784), name = 'X')
 y = tf.placeholder(tf.int64, shape = (None), name = 'y')
 
 X_ = tf.reshape(X, shape = [-1, 28, 28, 1])
+training = tf.placeholder_with_default(False, shape = (), name = 'training')
 
 with tf.name_scope('CNN'):
     C1 = tf.layers.conv2d(X_, filters = 6, kernel_size = 5, strides = [1, 1], padding = 'SAME')
-    C1_ = tf.tanh(C1)
-    S2 = tf.nn.avg_pool(C1_, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
-    S2_ = tf.tanh(S2)
+    C1_ = tf.nn.relu(C1)
+    C1_drop = tf.layers.dropout(C1_, 0.8, training=training)
+    S2 = tf.nn.avg_pool(C1_drop, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
+    S2_ = tf.nn.relu(S2)
     C3 = tf.layers.conv2d(S2_, filters = 16, kernel_size = 5, strides = 1, padding = 'VALID')
-    C3_ = tf.tanh(C3)
+    C3_ = tf.nn.relu(C3)
+    C3_drop = tf.layers.dropout(C3_, 0.3, training=training)
     S4 = tf.nn.avg_pool(C3_, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
-    S4_ = tf.tanh(S4)
+    S4_ = tf.nn.relu(S4)
     C5 = tf.layers.conv2d(S4_, filters = 120, kernel_size = 5, strides = [1, 1], padding = 'VALID')
-    C5_ = tf.tanh(C5)
+    C5_ = tf.nn.relu(C5)
 
 with tf.name_scope('FC'):
-    F6 = tf.layers.dense(C5_, 84, activation = tf.tanh)
+    F6 = tf.layers.dense(C5_, 84, activation = tf.nn.relu)
     logits = tf.layers.dense(F6, 10)
 
 with tf.name_scope('loss'):
@@ -44,6 +47,8 @@ saver = tf.train.Saver()
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/")
 
+X_valid = mnist.validation.images
+y_valid = mnist.validation.labels
 X_test = mnist.test.images
 y_test = mnist.test.labels
 
@@ -56,7 +61,11 @@ with tf.Session() as sess:
         for iteration in range(mnist.train.num_examples // batch_size):
             X_batch, y_batch = mnist.train.next_batch(batch_size)
             sess.run(training_op, feed_dict = {X: X_batch, y: y_batch})
+        training = True
         acc_train = accuracy.eval(feed_dict = {X: X_batch, y: y_batch})
-        acc_test = accuracy.eval(feed_dict = {X: X_test, y: y_test})
-        print(epoch, 'Train accuracy:', acc_train, 'Test accuracy:', acc_test)
-    save_path = saver.save(sess, './my_model_final.ckpt')
+        loss_train = loss.eval(feed_dict = {X: X_batch, y: y_batch})
+        training = False
+        acc_valid = accuracy.eval(feed_dict = {X: X_valid, y: y_valid})
+        print(epoch, 'Train accuracy:', acc_train, 'Train loss:', loss_train, 'Test validation:', acc_valid)
+    save_path = saver.save(sess, './my_model_final.ckpt'
+
